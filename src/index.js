b4b3c060beb1a932073fc8b1e1990e4196a18a23
Pedro Sanchez <113369857+Jules-Pelissou@users.noreply.github.com>
2023-11-08 09:36:00 +0000

    Working Forms

--- src/index.js
+++ src/index.js
@@ -8,8 +8,27 @@ var msg = myApp.printMsg(); // print and return " Hello World! "
 let express = require("express");
 let app = express();
 let port = 8080;
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/test", express.static("public"));
+
+app.use("/form", express.static("public/tva.html"));
+app.get("/tva", function (req, res) {
+  res.send(
+    `Prix HT :  ${req.query.HT} Prix TVA : ${req.query.tva}  Prix TTC : ${
+      req.query.HT * (1 + req.query.tva / 100)
+    }`,
+  );
+});
+
+app.post("/tva2", function (req, res) {
+  res.send(
+    `Prix HT :  ${req.body.HT} Prix TVA : ${req.body.tva}  Prix TTC : ${
+      req.body.HT * (1 + req.body.tva / 100)
+    }`,
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
