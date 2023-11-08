5b8afbf2004176abf284e58b59b5bf6ff12caa56
Pedro Sanchez <113369857+Jules-Pelissou@users.noreply.github.com>
2023-11-08 08:38:59 +0000

    Adding Express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 //exercice 0:
 console.log("message javascript du serveur");
+
+var myApp = require("simple-hello-world-example");
+
+var msg = myApp.printMsg(); // print and return " Hello World! "
+
+let express = require("express");
+let app = express();
+let port = 8080;
+
+app.use("/test", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
